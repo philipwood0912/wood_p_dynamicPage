45c702d8f2407dca661d7d91303e8b5a22a742a2
adam wood <a_wood60810@fanshaweonline.ca>
2019-03-13 01:18:20 -0400

    javascript complete

--- js/main.js
+++ js/main.js
@@ -1,1 +1,51 @@
-
+(() => {
+    
+    var textIndex = 1;
+    
+    let phil = document.querySelector(".phil"),
+        unknown = document.querySelector(".unknown"),
+        philButton = document.querySelector(".philip"),
+        unknownButton = document.querySelector(".second"),
+        arrowDown = document.querySelector(".oneA"),
+        arrowUp = document.querySelector(".twoA");
+        
+    
+    function showText(x) {
+        var textScroll = document.querySelectorAll(".textSwap");
+        if (x > textScroll.length) {
+            textIndex = 1;
+        }
+        if (x < 1) {
+            textIndex = textScroll.length;
+        }
+        for (i = 0; i < textScroll.length; i++) {
+            textScroll.forEach(text => {
+                text.classList.add("hidden");
+            });
+        }
+        textScroll[textIndex-1].classList.remove("hidden");
+    }
+    
+    unknown.classList.add("hidden");
+    
+    unknownButton.addEventListener("click", function() {
+        phil.classList.add("hidden");
+        unknown.classList.remove("hidden");
+    });
+    
+    philButton.addEventListener("click", function() {
+        phil.classList.remove("hidden");
+        unknown.classList.add("hidden");
+    });
+    
+    arrowDown.addEventListener("click", function() {
+        showText(textIndex += 1); 
+    });
+    
+    arrowUp.addEventListener("click", function() {
+        showText(textIndex -= 1);
+    });
+    
+    showText(textIndex);
+    
+})();
